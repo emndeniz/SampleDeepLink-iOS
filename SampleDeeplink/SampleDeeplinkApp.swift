e6f5fbfe5ecbedecad3b365c4ff9911a864a59e8
Emin DENİZ <emin.deniz@autoscout24.com>
2023-11-16 22:31:36 +0300

    Initial Commit

---
+++ SampleDeeplink/SampleDeeplinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDeeplinkApp.swift
+//  SampleDeeplink
+//
+//  Created by Mehmet Emin Deniz on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleDeeplinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
