af020bcac8d77459de0edacff1776c98d2a6b3c2
Emin DENİZ <emin.deniz@autoscout24.com>
2023-11-21 11:11:00 +0300

    Basic Deep link logic

--- SampleDeeplink/ContentView.swift
+++ SampleDeeplink/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter = 0
+    @State var lastUrl = "N/A"
+    @State var screenName = "Home"
+    @State var userType = "Default"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        VStack (spacing: 16, content: {
+            Text("Counter: \(counter)")
+            Text("Last URL:")
+            Text(lastUrl)
+            Divider()
+
+            Text("Screen : \(screenName)")
+            Text("User Type : \(userType)")
+        })
         .padding()
+        .onOpenURL(perform: { url in
+            parseUrl(url: url)
+        })
+    }
+}
+
+extension ContentView {
+    func parseUrl(url: URL){
+        counter += 1
+        lastUrl = url.absoluteString
+
+        let scheme = url.scheme
+        let host = url.host
+        let querry = url.query
+        print("Scheme: \(scheme), host: \(host), querryParams: \(querry)")
+
+        setRouting(host: host, querry: querry)
+
+    }
+
+    private func setRouting(host: String?, querry: String?){
+
+        switch host {
+        case "list":
+            screenName = "List Screen"
+        case "detail":
+            screenName = "Detail Screen"
+        default :
+            screenName = "Home Scren"
+        }
+
+        guard let querry = querry else {
+            userType = "Default"
+            return
+        }
+
+        switch querry {
+        case "paid":
+            userType = "Paid User 💰"
+        case "premium":
+            userType = "Premium User 🤑"
+        default :
+            userType = "Default User"
+        }
+
     }
 }
 
